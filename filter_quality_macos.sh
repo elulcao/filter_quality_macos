5474f13f247db713960b68b3bb1b749e223037a9
Daniel Carvallo <elulcao@icloud.com>
2021-03-19 11:45:06 -0600

    Subject: Initial commit Problem: Solution: Note:
    Signed-off-by: Daniel Carvallo <elulcao@icloud.com>

--- filter_quality_macos.sh
+++ filter_quality_macos.sh
@@ -22,8 +22,8 @@ $CP -- "$SOURCE" "$TARGET" # Copy macOS Filter to new directory
 
 # Overwrite the $TARGET file with custom values
 echo "Modifying target: $TARGET"
-$SED -i '' "28s/0.0/0.7/" "$TARGET" # Compression quality 0 - 1
-$SED -i '' "34s/0.0/0.7/" "$TARGET" # Scale factor 0 - 1
-$SED -i '' "38s/512/800/" "$TARGET" # SVGA Resolution
-$SED -i '' "40s/128/600/" "$TARGET" # SVGA Resolution
-$SED -i '' "48s/Reduce.*</Reduce\ File\ Size\ Quality</" "$TARGET" # Filter name
+$SED -i '' "s/0.0/0.6/" "$TARGET" # Compression quality 0 - 1
+$SED -i '' "s/0.5/0.6/" "$TARGET" # Scale factor 0 - 1
+$SED -i '' "s/512/800/" "$TARGET" # SVGA Resolution
+$SED -i '' "s/128/600/" "$TARGET" # SVGA Resolution
+$SED -i '' "s/Reduce.*</Reduce\ File\ Size\ Quality</" "$TARGET" # Filter name
